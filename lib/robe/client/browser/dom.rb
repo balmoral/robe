b3cc693154114431713c20c2d325127d890044f4
balmoral <colgunn@me.com>
2018-08-23 17:28:01 +1000

    rack and build improvements

--- lib/robe/client/browser/dom.rb
+++ lib/robe/client/browser/dom.rb
@@ -312,7 +312,7 @@ module Robe
 
         # the resolved hook will become a child of the element
         def resolve_hooked_content(element, hook)
-          trace __FILE__, __LINE__, self, __method__, " hook=#{hook}"
+          # trace __FILE__, __LINE__, self, __method__, " hook=#{hook}"
           element.hooks(init: true) << hook
           current_content = hook.value && sanitize_content(hook.value, element, coerce_to_element: true)
           hook.activate do |prior_state|
@@ -326,13 +326,13 @@ module Robe
         end
 
         def replace_hooked_content(element, new_content, old_content)
-          trace __FILE__, __LINE__, self, __method__, "(element: #{element}, new_content: #{new_content.class}:#{new_content}, old_content: #{old_content.class}:#{old_content}"
+          # trace __FILE__, __LINE__, self, __method__, "(element: #{element}, new_content: #{new_content.class}:#{new_content}, old_content: #{old_content.class}:#{old_content}"
           # trace __FILE__, __LINE__, self, __method__, ' ************** START WINDOW ANIMATION ON HOOK **************'
           window.animation_frame do
-            trace __FILE__, __LINE__, self, __method__, "(element: #{element.class}:#{element}, new_content: #{new_content.class}:#{new_content}, old_content: #{old_content.class}:#{old_content}"
+            # trace __FILE__, __LINE__, self, __method__, "(element: #{element.class}:#{element}, new_content: #{new_content.class}:#{new_content}, old_content: #{old_content.class}:#{old_content}"
             if old_content
               if new_content
-                trace __FILE__, __LINE__, self, __method__, "(element: #{element.class}:#{element}, new_content: #{new_content.class}:#{new_content}, old_content: #{old_content.class}:#{old_content}"
+                # trace __FILE__, __LINE__, self, __method__, "(element: #{element.class}:#{element}, new_content: #{new_content.class}:#{new_content}, old_content: #{old_content.class}:#{old_content}"
                 element.replace_child(new_content, old_content)
               else
                 element.remove_child(old_content)
